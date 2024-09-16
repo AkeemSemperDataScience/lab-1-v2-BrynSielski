ad369348344ef9eaad5b14e24f20874cf1f27594
BrynSielski <b.sielski@hotmail.com>
2024-09-16 10:19:07 -0600

    Worked on questions 4+5

--- START_Lab_1.py
+++ START_Lab_1.py
@@ -28,7 +28,7 @@ def lab1Question3(input_string, input_number):
     # Take in two inputs - a string and a number
     # Return the character of the string in the index given by number.  If this index does not exist, then return -1.
     character_at = -1
-    # need to take input_number's number and add 1 for correct spot since count starts at 0
+    # need to take input_number's number and subtract 1 for correct spot since count starts at 0
     character_at =  input_string[input_number - 1]
     # take that number and take input string's character are corrosponding place
 
@@ -37,11 +37,18 @@ def lab1Question3(input_string, input_number):
 
 def lab1Question4(file_name):
     # Take an input of a file name.
-    
     # Read that file and return a list of all numbers in that file
-    list_of_nums = []
+    #list_of_nums = [type(int), type(float)] #need to figure out how to read the file, extract numbers
+    # open file using "open" command
+    # define 'list_of_nums' as string from opened file
+    # extract floats and integers from 'a'
+    a = open(file_name)
+    #print(list_of_nums.read())
+    a.read()
+    list_of_nums = map(#floats and integers of 'a'
+    #print(a)
 
-    #place numbers into list_of_nums
+    #list_of_nums = file_name
 
     return list_of_nums
 
@@ -50,6 +57,10 @@ def lab1Question5(list_numbers):
     # Take an input of a list of numbers
     # Return the mode from that list. 
     mode_of_list = None
+
+    # find the mode of the original list of numbers (most common number)
+    # define mode_of_list to be the outcome of the count
+    mode_of_list = max(list_numbers, key=list_numbers.count)
 
     return mode_of_list
 
@@ -61,11 +72,6 @@ def lab1Question6(quarters, dimes, nickels, pennies):
     total = None
 
     total = ((quarters * .25) + (dimes * .1) + (nickels * .05) + (pennies * .01))
-
-    print("# of quarters:", quarters)
-    print("# of dimes:", dimes)
-    print("# of nickels:", nickels)
-    print("# of pennies:", pennies)
 
     return total
 
@@ -88,3 +94,31 @@ else:
 # Determining how to check if your code works, and define specific tests for what "working" means is 
 # something that allows you to tackele any larger problem - you can break it into smaller bits, attack one bit at a time,
 # check to ensure you've done it correctly, and then move on to the next bit.
+
+#question 2 check
+name = "Bryn"
+print("there is an odd amount of characters in", name, "-", lab1Question2(name))
+
+#question 3 check
+input_string = "Hello!"
+input_number = 2
+print("the", input_number, "stndrdth letter of", input_string, "is", lab1Question3(input_string, input_number))
+
+#question 4 check
+file_name = "Numbers.txt"
+print(lab1Question4(file_name))
+
+#question 5 check
+list_numbers = ["15", "17", "17", "14", "15", "16", "17"]
+print(lab1Question5(list_numbers))
+
+#question 6 check
+quarters = 4
+dimes = 3
+nickels = 2
+pennies = 1
+print("Your total is...", lab1Question6(quarters, dimes, nickels, pennies), "!")
+print("# of quarters:", quarters,"=", (quarters*.25))
+print("# of dimes:", dimes,"=", (dimes*.1))
+print("# of nickels:", nickels, "=", (nickels*.05))
+print("# of pennies:", pennies,"=", (pennies*.01)) 
